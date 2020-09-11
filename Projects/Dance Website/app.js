ff122157b1a1374618fd7c8bac0f4633f1bf570b
Sanket1228 <sanketpatil1228@gmail.com>
2020-09-11 15:36:02 +0530

    Web Developement Practices

--- Projects/Dance Website/app.js
+++ Projects/Dance Website/app.js
@@ -12,7 +12,10 @@ app.set('view engine','pug');                           //set the template engin
 app.set('views',path.join(__dirname,'views'));          //set the view directory
 
 app.get('/',(req,res)=>{
-    res.status(200).render('index.pug');
+    res.status(200).render('home.pug');
+})
+app.get('/contact',(req,res)=>{
+    res.status(200).render('contact.pug');
 })
 
 app.listen(port,()=>{
